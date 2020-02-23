45dfefb13cb19509df493dd0bac81ede0d451a5c
Christopher Swayne <chris_swa@icloud.com>
2020-02-23 12:37:04 -0600

    Setup for preliminary UI elements.
    Created the absolutely necessary activities and their layouts for the app.

---
+++ app/src/main/java/com/example/asymmetrical_android/EntryGenerationActivity.java
@@ -0,0 +1,14 @@
+package com.example.asymmetrical_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntryGenerationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_generation);
+    }
+}
