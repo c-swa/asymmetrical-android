0269234685ca0a8b448d31892bce6ac471acde0a
Christopher Swayne <chris_swa@icloud.com>
2020-03-29 11:46:07 -0500

    Folderview Designed
    Updated FolderView to use two recycler views to show Folders separate from notes

---
+++ app/src/main/java/com/example/asymmetrical_android/RootFolderView.java
@@ -0,0 +1,14 @@
+package com.example.asymmetrical_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RootFolderView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root_folder_view);
+    }
+}
