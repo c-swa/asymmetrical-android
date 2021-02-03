3a82398e031034aa5daec3c992dd36c408ccda02
Christopher Aram Swayne <42748779+c-swa@users.noreply.github.com>
2021-02-02 21:04:14 -0600

    Main Activity UI base complete

---
+++ app/src/main/java/com/example/asymmetrical_android/ThemeCreate.java
@@ -0,0 +1,14 @@
+package com.example.asymmetrical_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemeCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_create);
+    }
+}
